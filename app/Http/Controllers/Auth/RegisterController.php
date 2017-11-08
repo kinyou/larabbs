622598d721abd7c257e325e4f72d84f80bc45f89
wangxingyuan <nova_wangxy@126.com>
2017-11-08 13:37:59 +0800

    添加注册时的验证码

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -51,6 +51,18 @@ class RegisterController extends Controller
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:6|confirmed',
+	        'captcha'=>'required|captcha', //使用自定义表单验证规则https://d.laravel-china.org/docs/5.5/validation#using-extensions
+        ],[
+	        'name.required'=>'用户名不能为空',
+	        'name.max'=>'用户名最大长度不能超过255个字符',
+        	'email.required'=>'邮箱不能为空',
+        	'email.max'=>'邮箱最大长度不能超过255个字符',
+        	'email.unique'=>'邮箱已经被注册',
+        	'password.required'=>'密码不能为空',
+        	'password.min'=>'密码长度不能少于6位',
+        	'password.confirmed'=>'密码不一致',
+        	'captcha.required'=>'验证码不能为空',
+	        'captcha.captcha'=>'请输入正确的验证码'
         ]);
     }
 
